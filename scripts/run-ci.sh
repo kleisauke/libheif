28516ebe3d948e00a361fcfc6dee72a9d218e2f7
Dirk Farin <dirk.farin@gmail.com>
2025-03-02 21:25:00 +0100

    explicitly use mingw compiler

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -69,8 +69,8 @@ if [ "$MINGW" == "32" ]; then
     # Make sure the correct compiler will be used.
     unset CC
     unset CXX
-    CC=/usr/bin/x86_64-w64-mingw32-gcc-win32
-    CXX=/usr/bin/x86_64-w64-mingw32-g++-win32
+    export CC=/usr/bin/x86_64-w64-mingw32-gcc-win32
+    export CXX=/usr/bin/x86_64-w64-mingw32-g++-win32
     BIN_SUFFIX=.exe
     BIN_WRAPPER=wine
     export WINEPATH="/usr/lib/gcc/i686-w64-mingw32/9.3-posix/;/usr/i686-w64-mingw32/lib"
@@ -78,8 +78,8 @@ elif [ "$MINGW" == "64" ]; then
     # Make sure the correct compiler will be used.
     unset CC
     unset CXX
-    CC=/usr/bin/x86_64-w64-mingw32-gcc-win32
-    CXX=/usr/bin/x86_64-w64-mingw32-g++-win32
+    export CC=/usr/bin/x86_64-w64-mingw32-gcc-win32
+    export CXX=/usr/bin/x86_64-w64-mingw32-g++-win32
     BIN_SUFFIX=.exe
     BIN_WRAPPER=wine64
     export WINEPATH="/usr/lib/gcc/x86_64-w64-mingw32/9.3-posix/;/usr/x86_64-w64-mingw32/lib"
